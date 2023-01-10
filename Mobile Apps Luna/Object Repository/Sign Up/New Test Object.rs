f4773af2cb6556590f1a9347020d2627428d43d5
muhammadalharrys <muhamadalharrys@gmail.com>
2023-01-10 17:15:47 +0700

    mobile update

---
+++ Mobile Apps Luna/Object Repository/Sign Up/New Test Object.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>New Test Object</name>
+   <tag></tag>
+   <elementGuidId>5679bf1d-0c65-4901-aaba-1144821955c4</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
